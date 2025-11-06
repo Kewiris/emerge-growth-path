6a633b07379fcb062028469d912a9f29f9e3144d
Daniel Jabry <djabry@gmail.com>
2025-11-06 21:35:52 +0000

    Set base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? undefined : `/emerge-growth-path/`,
   server: {
     host: "::",
     port: 8080,
